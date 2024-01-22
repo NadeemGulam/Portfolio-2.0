ac4c3b62f3d828d8da3a4329ae844b8ce45082a3
Nadeem Gulam <68492875+NadeemGulam@users.noreply.github.com>
2024-01-22 21:11:47 +0530

    testing pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
-// import Header from './components/header/header.jsx' 
-// import Nav from './components/nav/nav.jsx'
-// import About from './components/about/About.jsx'
-// import Experience from './components/Experience/Experience.jsx'
-// import Services from './components/services/services.jsx'
-// import Portfolio from './components/portfolio/portfolio.jsx'
-// import Testimonial from './components/testimonial/testimonial.jsx'
-// import Contact from './components/Contact/Contact.jsx'
-// import Footer from './components/footer/footer.jsx'
+import Header from './components/header/header.jsx' 
+import Nav from './components/nav/nav.jsx'
+import About from './components/about/About.jsx'
+import Experience from './components/Experience/Experience.jsx'
+import Services from './components/services/services.jsx'
+import Portfolio from './components/portfolio/portfolio.jsx'
+import Testimonial from './components/testimonial/testimonial.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import Footer from './components/footer/footer.jsx'
 import MoreProjects from './components/moreProjects/moreProjects.jsx'
 const App = () => {
   return (
     <>
 
+<Header/>
+<Nav/>
+<About/>
+<Experience/>
+<Services/>
+<Portfolio/>
+<Testimonial/>
+<Contact/>
+<Footer/>
         <MoreProjects/>
     </>
     
