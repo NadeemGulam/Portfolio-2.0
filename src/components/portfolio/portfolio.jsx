646c8e17c4110d15613cb5703f1a66d6c6b49802
Nadeem Gulam <68492875+NadeemGulam@users.noreply.github.com>
2024-06-09 13:34:40 +0530

    Added some prpjects

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -5,12 +5,7 @@ import IMG2 from "./../../assets/portfolio2.jpg";
 import IMG3 from "./../../assets/portfolio3.jpg";
 import IMG4 from "./../../assets/portfolio4.jpg";
 import IMG5 from "./../../assets/portfolio5.jpg";
-import IMG6 from "./../../assets/portfolio6.jpg";
-import IMG7 from "./../../assets/portfolio7.png";
-import IMG8 from "./../../assets/portfolio8.png";
-import IMG9 from "./../../assets/portfolio9.png";
-import IMG10 from "./../../assets/portfolio10.png";
-import IMG11 from "./../../assets/portfolio11.png";
+
 import moreprojects from "./../../assets/moreprojects.jpg";
 import { Link } from "react-router-dom";
 
@@ -50,50 +45,7 @@ const data = [
     title: "React Based E-Commerce-Website",
     github: "https://github.com/NadeemGulam/React-Based-E-Commerce-Website",
     demo: "https://e-commerce-nadee-gulam.netlify.app/",
-  },
-  {
-    id: 6,
-    image: IMG6,
-    title: "Parking slot Detection using Arduino",
-    github:
-      "https://github.com/NadeemGulam/Parking-slot-detection-using-Arduino",
-    demo: "https://google.com",
-  },
-  {
-    id: 7,
-    image: IMG7,
-    title: "TinDog",
-    github: "https://github.com/NadeemGulam/TinDog",
-    demo: "https://nadeemgulam.github.io/TinDog/",
-  },
-  {
-    id: 8,
-    image: IMG8,
-    title: "Expense Tracker",
-    github: "https://github.com/NadeemGulam/Expense_tracker",
-    demo: "https://nadeem-expense-tracking.netlify.app",
-  },
-  {
-    id: 9,
-    image: IMG9,
-    title: "Snap Sign",
-    github: "https://github.com/NadeemGulam/Snap-Sign",
-    demo: "https://snap-sign.netlify.app",
-  },
-  {
-    id: 10,
-    image: IMG10,
-    title: "Library Book Seach Application",
-    github: "https://github.com/NadeemGulam/frontend-internship-assignment",
-    demo: "https://nadeem-gulam-fyle-intership.netlify.app",
-  },
-  {
-    id: 11,
-    image: IMG11,
-    title: "Quiz Application",
-    github: "https://github.com/NadeemGulam/Quiz-App",
-    demo: "https://quiz-app-nadeem-gulam.netlify.app",
-  },
+  }
 ];
 
 const portfolio = () => {
