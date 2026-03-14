22ca43ecc7fc20aadfbc73ad4258ac4f51796072
Nadeem Gulam <68492875+NadeemGulam@users.noreply.github.com>
2026-03-14 16:04:45 +0530

    feat: not found page 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Home from './components/home-page/home.jsx'
 import MoreProjects from "./components/moreProjects/moreProjects.jsx";
+import NotFound from "./components/NotFound/NotFound";
 import ChatBot from "./components/ChatBot/ChatBot";
 
 import { BrowserRouter,Routes,Route } from "react-router-dom";
@@ -12,6 +13,7 @@ const App = () => {
         <Routes>
           <Route path='' element={<Home/>} />
           <Route path='/more-projects' element={<MoreProjects/>} />
+          <Route path='*' element={<NotFound/>} />
         </Routes>
       </BrowserRouter>
       <ChatBot />
